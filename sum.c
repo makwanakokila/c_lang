245f2fd30d2e8a827335f906e4afe7464ab32f7f
makwanakokila <159900129+makwanakokila@users.noreply.github.com>
2024-02-15 14:36:14 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+main()
+{
+    int a,b,ans;
+    printf("enter value of a:");
+    scanf("%d",&a); 
+    printf("enter value of b:");
+    scanf("%d",&b);
+    ans=a+b;
+    printf("Addition of %d and %d is : %d\n",a,b,ans);
+    printf("%d + %d = %d\n\n",a,b,ans);
+
+    ans=a-b;
+    printf("Subtraction of %d and %d is : %d\n",a,b,ans);
+    printf("%d - %d = %d\n\n",a,b,ans);
+
+    ans=a*b;
+    printf("Multiplication of %d and %d is : %d\n",a,b,ans);
+    printf("%d * %d = %d\n\n",a,b,ans);
+
+    ans=a/b;
+    printf("Division of %d and %d is : %d\n",a,b,ans);
+    printf("%d / %d = %d\n\n",a,b,ans);
+
+    ans=a%b;
+    printf("Modulo of %d and %d is : %d\n",a,b,ans);
+    printf("%d % %d = %d\n\n",a,b,ans);
+
+}
